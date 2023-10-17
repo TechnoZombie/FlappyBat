65fe543d3ad75534e5efb372d31f8e13b9f2ae45
Telmo Sousa <telmo_sousa@live.com.pt>
2023-10-17 09:32:12 +0100

    Main to Telmo update from last night

---
+++ src/org/codeforall/shittyflappybird/EventHandler.java
@@ -0,0 +1,61 @@
+package org.codeforall.shittyflappybird;
+
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+public class EventHandler implements KeyboardHandler {
+
+    Obstacles pipes;
+    Character bird = new Character();
+
+
+   /* EventHandler(){
+         pipes = new Obstacles();
+    }
+*/
+
+    public void init() {
+        Keyboard kb = new Keyboard(this);
+
+        KeyboardEvent start = new KeyboardEvent();
+        start.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        start.setKey(KeyboardEvent.KEY_SPACE);
+        kb.addEventListener(start);
+
+        KeyboardEvent up = new KeyboardEvent();
+        up.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        up.setKey(KeyboardEvent.KEY_W);
+        kb.addEventListener(up);
+
+        KeyboardEvent down = new KeyboardEvent();
+        down.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        down.setKey(KeyboardEvent.KEY_S);
+        kb.addEventListener(down);
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        switch(keyboardEvent.getKey()){
+            case KeyboardEvent.KEY_SPACE:
+                break;
+            case KeyboardEvent.KEY_W:
+                bird.bird.translate(0, -50);
+                break;
+            case KeyboardEvent.KEY_S:
+                bird.bird.translate(0, 50);
+                break;
+
+
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+}
--- src/org/codeforall/shittyflappybird/Main.java
+++ src/org/codeforall/shittyflappybird/Main.java
@@ -4,8 +4,25 @@
 
 package org.codeforall.shittyflappybird;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Main {
     public static void main(String[] args) {
+
+        Rectangle grid = new Rectangle(10, 10, 500,700);
+        grid.setColor(Color.PINK);
+        grid.draw();
+        //  Picture arena = new Picture(0, 0, "Resources/waterfall.jpg");
+        //arena.draw();
+        new Obstacles().tryMove();
+        // EventHandler eventHandler = new EventHandler();
+        //eventHandler.init();
+
+
+
+
 
     }
 }
