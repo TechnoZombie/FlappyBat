ca2e84c80d8b728e3b0847985e03788cf5f9e116
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2023-10-18 15:20:16 +0100

    Revert "Antonio01"

--- src/org/codeforall/shittyflappybird/Main.java
+++ src/org/codeforall/shittyflappybird/Main.java
@@ -6,21 +6,16 @@ package org.codeforall.shittyflappybird;
 
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
-
-import static org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent.KEY_DOWN;
 
 public class Main {
     public static void main(String[] args) {
-
 
         Rectangle grid = new Rectangle(10, 10, 640, 1136);
         grid.setColor(Color.PINK);
         grid.draw();
 
-        Rectangle rec = new Rectangle();
-        rec.fill();
+   
 
         //new Character().run();
         Picture arena = new Picture(10, 10, "Resources/waterfall.jpg");
@@ -29,11 +24,7 @@ public class Main {
         EventHandler eventHandler = new EventHandler();
         Thread t = new Thread(eventHandler);
         t.start();
-        Enemies enemy = new Enemies();
-        Thread tt = new Thread(enemy);
-        tt.start();
-        enemy.spawnEnemiesRound1();
-
+        new Obstacles().tryMove();
 
     }
 }
