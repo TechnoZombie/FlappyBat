a54dd2ca8d73efe720c2cbc13733f121a1f82340
Sousa <telmo_sousa2003@hotmail.com>
2023-10-21 19:12:07 +0100

    Added a score/wave (rename it if you want) that increases every 5 seconds.

--- src/org/codeforall/shittyflappybird/GameScreen.java
+++ src/org/codeforall/shittyflappybird/GameScreen.java
@@ -10,11 +10,15 @@ package org.codeforall.shittyflappybird;
 
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
-import org.codeforall.shittyflappybird.GameObjects.Bat;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameScreen {
     private Picture arena;
+    private Text scoreText;
+    private Text scoreName;
 
 
     private int score;
@@ -27,13 +31,28 @@ public class GameScreen {
     }
 
     public void gridmaker() {
-        // picture ainda não existe, adicionar mais tarde
-
-        int sizeX = arena.getHeight(); //grid size fica dinamico com o tamanho da background image puxada no método backgroundImage()
+        int sizeX = arena.getHeight();
         int sizeY = arena.getWidth();
         Rectangle grid = new Rectangle(10, 10, sizeY, sizeX);
         grid.setColor(Color.CYAN);
         grid.draw();
+
+        // this is creating and initializing the score text
+        scoreName = new Text(1175, 25, "Wave:");
+        scoreName.setColor(Color.WHITE);
+        scoreName.grow(5,5);
+        scoreName.draw();
+
+
+        // this is creating and initializing the score text number
+        scoreText = new Text(1200, 45, String.valueOf(score));
+        scoreText.setColor(Color.WHITE);
+        scoreText.grow(3,3);
+        scoreText.draw();
+
+        // this is a task to update the score every 5 seconds
+        Timer timer = new Timer();
+        timer.schedule(new UpdateScoreTask(), 5000, 5000);
     }
 
     public void removeBackGround(){
@@ -44,31 +63,34 @@ public class GameScreen {
 
         score = 0;
         highScore = 0;
-       // bird = new Bat();
+        // bird = new Bat();
 
 
     }
 
-    // if condition, se o passaro nao colidir com as pipes/obstacles
-    // o score é aumentado em 1
-
-    /*public void updateGameState() {
-        if (!bird.collidesWith(obstacle)) {
-            updateScore(1);
-        }
-    }*/
 
     public void updateScore(int points) {
         score += points;
         if (score > highScore) {
             highScore = score;
         }
+        // this updates the score displayed on the screen
+        scoreText.setText(String.valueOf(score));
     }
 
     // usar quando dermos restart ao game=
     public void resetScore() {
         score = 0;
+        // this resets the displayed score to 0
+        scoreText.setText("0");
     }
 
+    private class UpdateScoreTask extends TimerTask {
+        @Override
+        public void run() {
+            // this is incrementing the score by 1 every 5 seconds
+            updateScore(1);
+        }
+    }
 
 }
